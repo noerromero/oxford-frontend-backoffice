0fb6c8af84a679ae0014e9a68c837834139847b8
noerromero <noerromero@gmail.com>
2025-01-14 23:09:50 -0500

    first version about validations of new student

--- src/views/students/newStudent.tsx
+++ src/views/students/newStudent.tsx
@@ -8,19 +8,30 @@ import {
 import GeneralErrorMessage from "../../components/shared/Error/GeneralErrorMessage";
 import { addStudent } from "../../services/students/StudentService";
 import StudentForm from "../../components/students/StudentForm";
+import * as v from "valibot";
+import { DraftStudentSchema } from "../../types/students";
 
 export async function action({ request }: ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData());
-  let error = "";
-  if (Object.values(data).includes("")) {
-    error = "Todos los campos son obligatorios";
-  }
-  if (error.length) {
+  console.log(data);
+  const result = v.safeParse(DraftStudentSchema, data);
+  console.log(result);
+  if (!result.success) {
+    const issues = v.flatten<typeof DraftStudentSchema>(result.issues);
+    const error = {
+      dni: issues.nested?.dni,
+      birthday: issues.nested?.birthday,
+      firstName: issues.nested?.name,
+      surname: issues.nested?.surname,
+      street: issues.nested?.street,
+      city: issues.nested?.city,
+      state: issues.nested?.state,
+    };
     return error;
   }
   await addStudent(data);
 
-  return redirect("/");
+  return redirect("/oxford/students");
 }
 
 export default function NewStudent() {
